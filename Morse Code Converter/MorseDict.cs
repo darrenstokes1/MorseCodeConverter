9c287185bb895a869e95067cc36db2ac5a685ec5
dagwoodnkauai <dagwoodnkauai@yahoo.com>
2016-11-29 18:49:13 -0500

    first commit

---
+++ Morse Code Converter/MorseDict.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morse_Code_Converter
+{
+    class MorseDict
+    {
+        // Create Dictionary
+
+        public Dictionary<char, string> lookupDictionary = new Dictionary<char, string>();
+
+        string fileName = "morse.csv";
+
+        public MorseDict()
+        {
+            using (var sr = new StreamReader(fileName))
+            {
+                while (sr.Peek() > 0)
+                {
+                    var line = sr.ReadLine().Split(',');
+                    var toLetter = Convert.ToChar(line[0]);
+                    var toMorse = line[1];
+                    lookupDictionary.Add(toLetter, toMorse);
+                    Console.WriteLine(line);
+                }
+
+            }
+
+
+        }
+
+        public string GetMorseCode(char letter )
+        {
+            Console.WriteLine($"getting morse code for {letter}"); // obtaining key for dictionary
+            return string.Empty;
+        }
+
+    }
+}
---
+++ Morse Code Converter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morse_Code_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Obtain input from user
+            Console.WriteLine("This program will convert your sentence to morse code and display it back to you.");
+            Console.WriteLine();
+            Console.WriteLine("Please enter one sentence.");
+            var userInput = Console.ReadLine().ToUpper().Replace(" ", "");
+            
+            var morseCode = new MorseDict();
+
+            foreach (var letter in userInput)
+            {
+                morseCode.GetMorseCode(letter); // Obtaining my key
+                
+
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
+
+
+
+
+
